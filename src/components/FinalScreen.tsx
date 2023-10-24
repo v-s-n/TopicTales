46954fd55ec5e753328d8472e60a1686fc997df3
sidharth-n <sidharthnofficial@gmail.com>
2023-10-24 21:57:00 +0530

    version 1

--- src/components/FinalScreen.tsx
+++ src/components/FinalScreen.tsx
@@ -7,10 +7,18 @@ interface FinalScreenProps {
 const FinalScreen: React.FC<FinalScreenProps> = ({ setCurrentStep }) => {
   return (
     <div className="px-4 relative flex flex-col items-center">
-      <h1 className="story-title font-bold">Final</h1>
-      <p className="story p-2 text-lg">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce euismod
-        odio vitae justo rutrum, in facilisis tellus aliquam.
+      <h1 className="story-title font-bold text-lg">
+        The Quantum Quest of Zippy Quark
+      </h1>
+      <p className="story p-2 text-base pb-8 ">
+        Zippy Quark was a teeny tiny particle who lived in an odd atomic world
+        where strange things happened. One day, as Zippy was spinning around,
+        his friend Wavey said, "Slow down! I can't see you clearly when you move
+        so fast." Zippy grinned, "But I'm both a particle and a wave! When I
+        spin super fast, my wave nature takes over and I become a blurry smear."
+        "You're weird, Zippy," Wavey chuckled. "It's one of your quantum
+        quirks." Later, Zippy got zapped into a cage made of impenetrable metal
+        walls. "Oh no!" he cried. "I'm trapped!" He leaned against a wall and
       </p>
     </div>
   );
